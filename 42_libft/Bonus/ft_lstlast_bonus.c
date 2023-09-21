6acaa3701e7756817303893e3d6e8b34260cc228
Younes Es-Slimani <yes-slim@c4r2p6.1337.ma>
2023-09-21 18:05:46 +0100

    stating RC

---
+++ 42_libft/Bonus/ft_lstlast_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 10:12:06 by yes-slim          #+#    #+#             */
+/*   Updated: 2022/10/31 23:53:09 by yes-slim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*tmp;
+
+	tmp = lst;
+	while (tmp != NULL)
+	{
+		if (tmp->next == NULL)
+			break ;
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
